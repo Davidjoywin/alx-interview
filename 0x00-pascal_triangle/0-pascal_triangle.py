e07ba7ad48d81be07df4f911e43a559a17a8a777
David Joy <davidjoy.win@gmail.com>
2023-07-02 02:45:27 +0100

    interview question done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""pascal's triangle"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    outer = []
+
+    for i in range(1, n+1):
+        if i <= 2:
+            inner = []
+            for _ in range(i):
+                inner.append(1)
+            outer.append(inner)
+
+        else:
+            inner = []
+            prev = outer[i-2]
+            inner.append(1)
+            for j in range(len(prev)-1):
+                inner.append(prev[j] + prev[j+1])
+            inner.append(1)
+            outer.append(inner)
+    return outer
+
